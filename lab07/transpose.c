ad19a7bca395ba237dba9c17943fc662f8775193
huaeryi <917581079@qq.com>
2023-03-04 15:08:39 +0800

    lab07

--- lab07/transpose.c
+++ lab07/transpose.c
@@ -16,6 +16,19 @@ void transpose_naive(int n, int blocksize, int *dst, int *src) {
  * multiple of the block size. */
 void transpose_blocking(int n, int blocksize, int *dst, int *src) {
     // YOUR CODE HERE
+    for (int i = 0; i < n; i += blocksize) {
+        for (int j = 0; j < n; j += blocksize) {
+            for (int x = 0; x < blocksize; x++) {
+                for (int y = 0; y < blocksize; y++) {
+                    int line = i + x;
+                    int col = j + y;
+                    if (line < n && col < n) {
+                        dst[n * col + line] = src[n * line + col];
+                    }
+                }
+            }
+        }
+    }
 }
 
 void benchmark(int *A, int *B, int n, int blocksize,
